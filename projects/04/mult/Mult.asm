bdef77677303a55464f81aad2da789182ec34f8b
snufkon <snufkon@gmail.com>
2015-04-22 07:54:31 +0900

    implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,44 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+        // i = R1
+        @R1
+        D=M
+        @i
+        M=D
+
+        // sum = 0
+        @sum
+        M=0  
+(LOOP)
+        // if i == 0 then jump to END
+        @i
+        D=M
+        @END
+        D;JEQ
+
+        // sum = sum + R0
+        @R0
+        D=M
+        @sum
+        M=M+D
+
+        // i = i - 1
+        @i
+        M=M-1
+
+        // jump to LOOP
+        @LOOP
+        0;JMP
+        
+(END)
+        // R2 = sum
+        @sum
+        D=M
+        @R2
+        M=D
+
+        // exit program
+        @END
+        0;JMP
